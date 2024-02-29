6eb667ecd5ea907feceb5b5a8ee161d807061df6
SURYA <160419610+suryaraja21@users.noreply.github.com>
2024-02-29 22:01:05 +0530

    Add files via upload

---
+++ transpose the matrix.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+main(){
+    int r,c,i,j;
+    printf("Enter the size of matrix");
+    scanf("%d%d",&r,&c);
+    int matrix[r][c];
+    printf("\n Enter the values:\n");
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    printf("\n");
+    for(i=0;i<c;i++){
+        for(j=0;j<r;j++){
+            printf("%d ",matrix[j][i]);
+        }
+        printf("\n");
+    }
+}
